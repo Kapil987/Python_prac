1eb9e6f8a6e7d0ccf690c66f7f93d041526b5c95
Kapil <Kapil>
2020-05-08 15:50:24 +0530

    changed master branch name to tut_python

--- kt_python/tut13_if_else.py
+++ kt_python/tut13_if_else.py
@@ -1,59 +1,69 @@
 # if elif else
 
-# var1 = 10
-# var2 = 20
+var1 = 10
+var2 = 20
 
 # print("Enter a value ;")
 # var3 = int(input())
 
+# Simple If else
 # if var3>var2:           #Check one
 #     print("Greater")
-# if var3 == var2:        #Check two
+# else:
+#     print("Lesser")
+
+
+#Problem
+# if var3>var2:           #Check one
+#     print("Greater")
+
+# if var3 == var2:        #Check two Multiple checks
 #     print("Equal")
 # else:
 #     print("Lesser")
 
-# if var3>var1:
+# Solution
+# if var3>var2:           
 #     print("Greater")
-# elif var3==var1:
-#     print("equal")
+# elif var3 == var2:       
+#     print("Equal")
 # else:
 #     print("Lesser")
 
+
 # # Switch Case implementation using Dictionary
-switch = {0:'Zero',1:'One',2:'Two'}
+# switch = {0:'Zero',1:'One',2:'Two'}
 # print(switch[int(input("Enter a number: "))])
+# switch[0]
 
 # if else implementation on list and Dictionary
-list = [1, 2, 3, 4]
-# if var3 in list1:
+list1 = [1, 2, 3, 4]
+# print("Enter a value ;")
+# var3 = int(input())
+
+# if var3 in list1:           # in keyword
 #     print("Yes in list")
 # else:
 #     print("Not in list")
 
 # if var3 not in list1:     # not and in are two keywords here
-#     print("True")
+#     print("Condition True")
 # else:
-#     print("False")
+#     print("Condition False")
+
 # The above can be replace by print
 # print(var3 not in list1)      # output is form of boolean
 
-inp_var = int(input("Enter a value to be searched: "))
-if inp_var not in list:             # in and not are different keywords
-    print("Present in list")
-else:
-    print("Not present in list")
-
 
 # Quiz
-# print("What is your age?")
-# age = int(input())
-# if age<18:
-#     print("You cannot drive")
-# elif age == 18:
-#     print("We will think about you")
-# else:
-#     print("You can drive")
+print("What is your age?")
+age = int(input())
+if age<18:
+    print("You cannot drive")
+elif age == 18:
+    print("We will think about you")
+else:
+    print("You can drive")
 
 # but if you enter 7777 it will say you can drive this is Problem!!!
 
