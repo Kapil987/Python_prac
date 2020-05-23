f0a01ce295888f3ae066f98cef9f7527f0835f2f
Kapil <Kapil>
2020-05-23 13:58:06 +0530

    added enumerate fun usage

--- kt_python/tut36_Anonymous_Lambda_Functions.py
+++ kt_python/tut36_Anonymous_Lambda_Functions.py
@@ -8,8 +8,29 @@
 ##
 # def str_fun(strs):
 #     return strs[-1]
+# ab = lambda strs:strs[-1]
+# print(ab(strs))           # str = wa 0=w 1 =a , -1=a
 # strs = ['xc', 'zb', 'yd' ,'wa']
+# strs = ['xc', 'zb', 'yz' ,'wy']
 # simple = ['c','b','a','z']
 # print(sorted(simple))
+# print(str_fun(strs))
+
 # print(sorted(strs,key=lambda strs: strs[-1]))
 
+votes = {'Charlie': 20, 'Able': 10, 'Baker': 20, 'Dog': 15}
+#If we apply .items() on the votes dictionary above we get:
+#[('Charlie', 20), ('Baker', 20), ('Able', 10), ('Dog', 15)]
+# 0(0        , 1), 1(0     , 1 ), 2(0     , 1 ),3(0     , 1 )
+#a list of tuples, each tuple having two items indexed 0 and 1
+list_1 = [votes.items()]
+tup1 = (list_1)
+# print("list: ",list_1)
+# print("tup: ",tup1)
+# print(sorted(votes.items(),key=lambda x:x[1]))
+#lambda x = ('Charlie', 20) or ('Baker', 20) or ('Able', 10) or ('Dog', 15)
+# x[1]  =               20  or           20  or          10  or         15
+# Where there is a tie in the key, the items are ranked in the order they are originally 
+# in the dictionary. (so ('Charlie', 20) is before ('Baker', 20) because there is a 20==20 
+# tie on the key but ('Charlie', 20) comes before ('Baker', 20) in the original votes 
+# dictionary).
--- kt_python/tut41_arg_and_kwargs.py
+++ kt_python/tut41_arg_and_kwargs.py
@@ -1,13 +1,15 @@
+## 1
 def fun1(a, b, c, d):
     print(a, b, c, d)
 
-fun1("Amar","Akbar","Anthony","Amit") # unable to give 5 arguments here
+# fun1("Amar","Akbar","Anthony","Amit") # unable to give 5 arguments here
 
+## 2
 ## function(normal_argument, *args, **kwargs) sequence is convenstion
-def fun_arg(str_class,*args, **kwargs): # args is just convention we can change this name, keep args after str_class
-    print(type(args))         # else error
+def fun_arg(str_class,*args,**kwargs): # args is just convention we can change this name, keep args after str_class
+    # print(type(args))         # else error
     print(str_class)
-    # print(args[0])
+    # print("args: ",args[0])
     for i in args:
         print(i)
     print("Staff\n")
@@ -17,4 +19,5 @@ def fun_arg(str_class,*args, **kwargs): # args is just convention we can change 
 list_str = ["Amar","Akbar","Anthony","Amit"]
 str_class = "These are student of class 9:"
 kw = {"Sohan":"Head", "Ram":"Teacher","Kapil":"Programmer"}
+# fun_arg(str_class,*list_str,**kw)
 fun_arg(str_class,*list_str,**kw)
