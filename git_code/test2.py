63e432fe20d7309f52394b4d7961ce0be83e2665
kapil <kapil0123@gmail.com>
2022-05-27 08:32:00 +0530

    folder re-arrangements

--- git_code/test2.py
+++
@@ -1,8 +0,0 @@
-Num1 = int(input("enter the first number:"))
-Num2 = int(input("enter the second number:"))
-print("Sum of two numbers:",Num1+Num2)
-print("Substraction of two numbers:",Num1-Num2)
-print("Multiply of two numbers:",Num1*Num2)
-print("Division of two numbers:",Num1/Num2)
-print("Modulus of two numbers:",Num1%Num2)
-print("program end")
